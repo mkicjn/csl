797219ad1a5da804cf538f584c42ea60a18066c9
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-01-23 16:22:33 -0600

    Fixed memory management of RPN translator

--- rpn.h
+++ rpn.h
@@ -1,20 +1,27 @@
 #ifndef	RPN_H
 #define RPN_H
 #include "lisp.h"
+#include "stack.h"
+obj_t *rpnh(obj_t *);
 obj_t *rpn(obj_t *form)
+{
+	obj_t *r=rpnh(form);
+	dec_rc(form);
+	return r;
+}
+obj_t *rpnh(obj_t *form)
 {
 	if (form->type!=CELL)
 		return cons(form,NIL);
 	push(NULL);
 	for (obj_t *o=cdr(form);o!=NIL;o=cdr(o))
-		push(rpn(car(o)));
-	push(rpn(car(form)));
+		push(rpnh(car(o)));
+	push(rpnh(car(form)));
 	while (stackitem(1))
 		s_nconc();
 	obj_t *r=pop();
 	pop(); // Drop NULL
-//	r->refs-=r->refs>0;
-//	dec_rc(form);
+	r->refs--;
 	return r;
 }
 #endif
