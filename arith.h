dfda160d7b32180f11ad63975d9ccb49952b4e83
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-01-30 16:23:20 -0600

    Added error checking to arithmetic

--- arith.h
+++ arith.h
@@ -1,8 +1,14 @@
 #ifndef ARITH_H
 #define ARITH_H
 #include "lisp.h"
+static bool is_num(obj_t *obj)
+{
+	return obj->type==INTEGER&&obj->type==DOUBLE;
+}
 core(+,2) add(obj_t *a,obj_t *b)
 {
+	if (!is_num(a)||!is_num(b))
+		return &ERROR_OBJ;
 	if (a->type==INTEGER&&b->type==INTEGER)
 		return new_obj(INTEGER,a->car+b->car,0);
 	double n1,n2;
@@ -12,6 +18,8 @@ core(+,2) add(obj_t *a,obj_t *b)
 }
 core(-,2) sub(obj_t *a,obj_t *b)
 {
+	if (!is_num(a)||!is_num(b))
+		return &ERROR_OBJ;
 	if (a->type==INTEGER&&b->type==INTEGER)
 		return new_obj(INTEGER,a->car-b->car,0);
 	double n1=a->type==INTEGER?(double)a->car:((dobj_t *)a)->car;
@@ -20,6 +28,8 @@ core(-,2) sub(obj_t *a,obj_t *b)
 }
 core(*,2) mult(obj_t *a,obj_t *b)
 {
+	if (!is_num(a)||!is_num(b))
+		return &ERROR_OBJ;
 	if (a->type==INTEGER&&b->type==INTEGER)
 		return new_obj(INTEGER,a->car*b->car,0);
 	double n1,n2;
@@ -29,6 +39,8 @@ core(*,2) mult(obj_t *a,obj_t *b)
 }
 core(/,2) divd(obj_t *a,obj_t *b)
 {
+	if (!is_num(a)||!is_num(b))
+		return &ERROR_OBJ;
 	if (a->type==INTEGER&&b->type==INTEGER&&a->car%b->car==0)
 		return new_obj(INTEGER,a->car/b->car,0);
 	double n1=a->type==INTEGER?(double)a->car:((dobj_t *)a)->car;
