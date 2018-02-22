00a449489281673a50714989df2c83b0ad15b16f
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-02-21 23:40:16 -0600

    Removed comment from format example

--- format.lisp
+++ format.lisp
@@ -4,7 +4,6 @@
   (declare 'sp '" ")
   (declare 'colon ':)
   (declare 'backslash '\)
-  '"(declare 'format (lambda '(n) '(mapcar print n)))"
   (declare 'format (lambda '(f) '(cond (f (progn (print (car f)) (@ (cdr f)))))))
   (declare 'space (lambda '(l) '(cond ((cdr l) `(:(car l) :sp \(@ (cdr l)))) (t (list (car l))))))
   t)
